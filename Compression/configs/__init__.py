36adc710248c56a72581634e7eb8f4bc9da35585
Simon <simon_wzm@163.com>
2023-12-03 12:41:38 +0800

    only env set

--- Compression/configs/__init__.py
+++
@@ -1,8 +0,0 @@
-def encode_config(config):
-    return '_'.join([str(c) for c in config['channels']])
-
-
-def decode_config(config_str):
-    channels = config_str.split('_')
-    channels = [int(c) for c in channels]
-    return {'channels': channels}
--- Compression/train.py
+++
@@ -1,6 +0,0 @@
-from trainer import Trainer
-
-if __name__ == '__main__':
-    trainer = Trainer('train')
-    trainer.start()
-    print('Training finished!!!')
